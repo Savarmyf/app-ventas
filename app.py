cac2dd3808ae559372618891ef50a2c3be968855
Savarmyf <93285801+Savarmyf@users.noreply.github.com>
2026-02-05 16:56:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -147,8 +147,8 @@ seccion = st.radio(
     "Ir a:",
     ["📊 Dashboard", "🗓 Registro", "🛒 Ventas", "💰 Balance", "🌳 Red", "📝 Notas"],
     label_visibility="collapsed"
-
     )
+
     st.link_button("📘 Guía técnica", GUIA_DRIVE_URL)
     if st.button("🚪 Cerrar sesión"):
         st.session_state.usuario = None
@@ -354,4 +354,5 @@ elif seccion == "📝 Notas":
         notas[usuario] = nota_nueva
         guardar_data(data, sha)
         st.success("Notas guardadas")
+
 
